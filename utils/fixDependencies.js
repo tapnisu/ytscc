a449bd2717c958a3d3e9797c3d8221040717b346
Alexiy Rybin <alexiy.rybin@gmail.com>
2022-05-15 18:51:07 +0700

    Formated code

--- utils/fixDependencies.js
+++ utils/fixDependencies.js
@@ -6,9 +6,12 @@ const path = require('path')
 
 const fileDir = `${path.dirname(require.resolve('youtube-chat'))}/parser.js`
 
-readFile(fileDir, {encoding: 'utf8'}, function (err,data) {
-	var formatted = data.replace('const shortcut = run.emoji.shortcuts[0];', 'const shortcut = (run.emoji?.shortcuts?.length > 0) ? run.emoji?.shortcuts[0] : "";')
-	
+readFile(fileDir, { encoding: 'utf8' }, function (err, data) {
+	var formatted = data.replace(
+		'const shortcut = run.emoji.shortcuts[0];',
+		'const shortcut = (run.emoji?.shortcuts?.length > 0) ? run.emoji?.shortcuts[0] : "";'
+	)
+
 	writeFile(fileDir, formatted, 'utf8', function (err) {
 		if (err) return console.log(err)
 	})
